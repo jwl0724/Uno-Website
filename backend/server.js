7e00999cfebe0a48aad2f5455df116a8fa1119cc
Jonathan Liu <jwliu0724@hotmail.com>
2024-01-16 16:01:45 -0800

    add create room in backend

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,24 @@ const app = require('express')();
 const cors = require('cors');
 const PORT = 3000;
 
+function generateRoomId() {
+    return Math.floor(Math.random() * 10000).toString();
+}
+
 app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Now listening to port ${PORT}`);
 });
 
-app.get('/create', async (req, resp) => {
+app.get('/createRoom', async (req, resp) => {
+    // create room
+    let playerCount = req.query.playerCount;
     
+    // add player to room
+    
+
+    // return roomId
+    let roomId = generateRoomId();
+    resp.send({ roomId: roomId });
 })
